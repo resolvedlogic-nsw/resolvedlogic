ef1affa9c05a256277aaf194f25990d57fc7a1ec
resolvedlogic-nsw <resolvedlogic@gmail.com>
2026-08-26 12:21:24 +1000

    Remove Django app: split into private resolvedlogic-django repo
    contracts/, pages/, pages_html/, resolvedlogic_django/, templates/,
    manage.py and requirements.txt moved to a new private repo since the
    contracts app stores client PII (names, phone numbers, signatures,
    IP addresses) that shouldn't sit in a public repo. This repo goes
    back to being just the static marketing site for GitHub Pages.

--- pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.home_redirect, name="home_redirect"),
-    path("<str:filename>", views.serve_page, name="serve_page"),
-]
